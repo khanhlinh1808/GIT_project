b376fcc78e2b5b422840546ad1768aebb8e20185
Đoàn Cường <doancuong@MacBook-Air-cua-oan.local>
2022-04-13 15:36:55 +0700

    new momentjs

---
+++ main.js
@@ -0,0 +1,16 @@
+const a = document.querySelector('a')
+for (let i = 0; i < 3; i++) {
+     console.log(i)
+     setTimeout(() => {
+          console.log('timeout run')
+          console.log(i)
+     }, 1)
+}
+const greeting = {
+     title: 'Hello world !',
+     color: 'red',
+}
+
+function a() {
+     const a = 'anme'
+}
